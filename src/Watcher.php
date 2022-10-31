a5112eb290ffb38dd86dadf186437a4b13e041a1
Magomedcoder <info@magomedcoder.ru>
2022-10-31 22:55:22 +0300

    first commit

---
+++ src/Watcher.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Watcher;
+
+/**
+ * Class Watcher
+ * @package PCore\Watcher
+ * @github https://github.com/pcore-framework/json-rpc
+ */
+class Watcher
+{
+
+}
